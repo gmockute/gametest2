bc1d9f71a35eb1b080cb8b1f824f52c09f07c33e
Gintare <gintare.mocku@gmail.com>
2023-05-07 14:26:00 +0300

    Added urls to methods

--- src/main/java/lt/gintare/gametest/challenge/controller/ChallengeController.java
+++ src/main/java/lt/gintare/gametest/challenge/controller/ChallengeController.java
@@ -16,24 +16,29 @@ public class ChallengeController {
     @Autowired
     ChallengeService challengeService;
 
-    // http://localhost:8080/game/random/opponent
+    // METHOD 1 -> get random opponent details
+    // http://localhost:8080/game/challenge/random/opponent
     @GetMapping(path = "/random/opponent")
     public Opponent getRandomOpponent (Model model){
         return challengeService.getOpponent();
     }
 
-    // METHOD 2: displays player name (entry in page 2) if new player or select from database if existing player
-    // challenge page
+    // METHOD 2 -> displays player name (entry in page 2) if new player or select from database if existing player
+    // http://localhost:8080/game/challenge/player
 
-    // METHOD 3: upon button1 press calls method in ChallengeService to execute challenge1
+    // METHOD 3 -> upon button1 press calls method in ChallengeService to execute challenge1
     // if result is 2 or 3 - loads win page and if 0 or 1 - loads loose page
+    // http://localhost:8080/game/challenge/execute_action
 
     // METHOD 4: upon button2 press calls method in ChallengeService to execute challenge2
     // if result is 2 or 3 - loads win page and if 0 or 1 - loads loose page
+    // http://localhost:8080/game/challenge/execute_noaction
 
     // METHOD 5: displays result in win and loose pages
+    // http://localhost:8080/game/challenge/result
 
     // METHOD 6: displays player name (entry in page 2) if new player or select from database if existing player
     // win page and loose page
+    // http://localhost:8080/game/challenge/name
 
 }

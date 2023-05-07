dd1e7e6aa1f11d9fd22138755e18e2442f7700f9
Gintare <gintare.mocku@gmail.com>
2023-05-07 16:02:03 +0300

    Amended query method.

--- src/main/java/lt/gintare/gametest/challenge/repository/OpponentRepository.java
+++ src/main/java/lt/gintare/gametest/challenge/repository/OpponentRepository.java
@@ -3,12 +3,14 @@ package lt.gintare.gametest.challenge.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface OpponentRepository extends CrudRepository <Opponent, Integer> {
 
     // QUERY 1 -> gets random opponent and puts it in a list
-    @Query(value = "SELECT * FROM game_repository.opponent ORDER BY RAND() LIMIT 1", nativeQuery = true)
-    List<Opponent> findRandomOpponent();
+    @Query(value = "SELECT opponent_id, " +
+                   "opponent_name, " +
+                   "opponent_role, " +
+                   "opponent_description" +
+            " FROM game_repository.opponent ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Opponent getRandomOpponentDetails();
 
 }
